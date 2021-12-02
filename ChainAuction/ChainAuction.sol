95b62a92c4620c33739f14ada91cd217ed72cdfd
Chris Fox <cheopys@gmail.com>
2021-12-02 18:16:31 +0700

    testing Github Desktop

--- ChainAuction/ChainAuction.sol
+++ ChainAuction/ChainAuction.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.8.0;
 
 contract Auction
@@ -13,84 +11,56 @@ contract Auction
     struct Bid
     {
         Bidder  bidder;
+        uint32  itemId;
         uint256 amount;
-        uint256 date;
+        uint32  date;
     }
 
     struct Item
     {
+        uint32  itemId;
         string  description;
         uint256 initialPrice;
-        uint256 datePosted;
-        uint256 dateExpiration;
+        uint32  datePosted;
+        uint32  dateExpiration
         Bid     bidHighest;
     }
     
     struct Seller
     {
-        string   email;
-        uint32[] itemIds;
+        address addressBidder;
+        string  email;
+        Item[]  items;
     }
     
     uint32  private itemCount;
-    mapping(uint32 => Item) public Items;
-    mapping(address => Bidder) public  Bidders;
-    mapping(address => Seller) public  Sellers;
+    mapping public  Items(uint32 => Item);
+    mapping public  Bidders(address => Bidder);
     address private administrator;
-    Bid private emptyBid;
     
     constructor()
     {
         administrator = msg.sender;
-        emptyBid      = Bid(Bidder(msg.sender, ""), 0, 0);
     }
-
-    modifier isAdministrator() 
-    {
-        require(msg.sender == administrator);
-        _;
-    }
-
-    function addSeller(address sellerNew, string memory emailNew) public isAdministrator
-    {
-        Seller memory seller;
-        
-        seller.email = emailNew;
-        
-        Sellers[sellerNew] = seller;
-    }
-
-    // called by seller
     
-    function addItem(string  calldata description, 
-                     uint256 price, 
-                     uint256 expiration) public 
+    function addItem(string email, string description, uint256 price, uint32 expiration) public 
     {
         require(expiration > block.timestamp);
         
-        uint32 itemIdNew = itemCount++;
-        Seller storage seller    = Sellers[msg.sender];
-        Items[itemIdNew] = Item({description: description, initialPrice: price, datePosted: block.timestamp, dateExpiration: expiration, bidHighest: emptyBid});
-        seller.itemIds[seller.itemIds.length] = itemIdNew;
-    }
-    
-    // called by Bidder
-    
-    function placeBid(uint32 itemId, string calldata emailBidder) public payable returns (bool)
-    {
-        Item storage item = Items[itemId];
-        bool highest = false;
-
-        if (msg.value > item.bidHighest.amount)
+        Bidder bidder = bidders[msg.sender];
+        
+        if (bidder.email == "")
         {
-            item.bidHighest.bidder.addressBidder = msg.sender;
-            item.bidHighest.bidder.email         = emailBidder;
-            item.bidHighest.amount = msg.value;
-            item.bidHighest.date   = block.timestamp;
-
-            highest = true;
+            bidder.email = email
         }
         
-        return highest;
+        bidder.Items.push(Item({iotemId: itemCount++, descrption: description, initialPrice: price, datePosted: block.timestamp, dateExpiration: expiration}))
     }
+
+    // called by external JS code on clock tick
+
+    function processExpiredAuctions() public returns (uint)
+    {
+    }
+
 }
