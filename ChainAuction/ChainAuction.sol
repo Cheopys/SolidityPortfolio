88a4e1941d4784f43746b9117bee27e62fc866f0
Chris Fox <cheopys@gmail.com>
2021-12-03 23:16:20 +0700

    pay sellers

--- ChainAuction/ChainAuction.sol
+++ ChainAuction/ChainAuction.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-contract Auction
+contract ChainAuction
 {
     struct Bidder
     {
@@ -28,21 +28,37 @@ contract Auction
     
     struct Seller
     {
+        address payable addressSeller;
         string   email;
         uint32[] itemIds;
+        uint32[] itemIdsHistory;
     }
     
+    struct EtherOwedToSeller
+    {
+        uint256 amountOwed;
+        address payable sellerOwed;
+    }
+
     uint32  private itemCount;
     mapping(uint32 => Item)    public Items;
     mapping(address => Bidder) public Bidders;
     mapping(address => Seller) public Sellers;
-    Item[]                     public ItemsHistory;
+    mapping(uint32 => Item)    public ItemsHistory;
+
+    // administrator
+
     address private administrator;
-    
+    EtherOwedToSeller[] private etherOwed;
+
+    Item[] private ItemsRemoved;
+
     constructor()
     {
         administrator = msg.sender;
     }
+
+    event EtherOwedSeller(uint256 amount, Seller seller);
 
     modifier isAdministrator() 
     {
@@ -50,13 +66,28 @@ contract Auction
         _;
     }
 
+    // administrator functions
+
     function addSeller(address sellerNew, string memory emailNew) public isAdministrator
     {
         Seller memory seller;
         
+        seller.addressSeller = address;
         seller.email = emailNew;
         
         Sellers[sellerNew] = seller;
+    }
+
+    function paySellers() public payable isAdministrator
+    {
+        while (etherOwed.length > 0)
+        {
+            EtherOwedToSeller storage owed = etherOwed[etherOwed.length - 1];
+
+            owed.sellerOwed.transfer(owed.amountOwed);
+
+            etherOwed.pop();
+        }
     }
 
     // SELLER functions
@@ -97,8 +128,8 @@ contract Auction
 
         uint256 iItem = seller.itemIds.length;
         uint256 amount;
-        Item[] memory items = new Item[](seller.itemIds.length);
-        uint256 iItemRemoved;
+
+        delete ItemsRemoved;
 
         do 
         {
@@ -113,13 +144,38 @@ contract Auction
                     item.bidHighest.bidder.addressBidder.transfer(item.bidHighest.amount);
                 }
 
-                items[iItemRemoved++] = item;
-                delete Items[itemId];
-                ItemsHistory.push(item);
+                moveItemToHistory(seller, item);
             }
         } while (iItem > 0);
 
-        return items;
+        Item[] memory ItemsRemovedMemory = ItemsRemoved;
+
+        // alert administrator to pay seller
+
+        if (amount > 0)
+        {
+            EtherOwedToSeller storage owed = EtherOwedToSeller({amountOwed: amount, 
+                                                                sellerOwed: payable(seller.addressSeller)});
+            etherOwed.push(EtherOwedToSeller(owed));
+            emit EtherOwedSeller(amount, seller);
+        }
+
+        delete ItemsRemoved;
+
+        return ItemsRemovedMemory;
+    }
+
+    function moveItemToHistory(Seller storage seller, Item storage item) private
+    {
+        // move between state variables
+
+        ItemsHistory.push(item);
+        delete Items[item.itemId];
+
+        // move betweeen seller variables
+
+        seller.itemIdsHistory.push(item.itemId);
+        ItemsHistory[item.itemId].push(item);
     }
 
     // called by Bidder
